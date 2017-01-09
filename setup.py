8c924c9e89fd0da8b26c9d4b0e40ae7e94aefac1
David Caro <david@dcaro.es>
2017-01-09 14:54:25 +0100

    travis: dropped 3.3 and 2.6 python builds
    Signed-off-by: David Caro <david@dcaro.es>

--- setup.py
+++ setup.py
@@ -80,10 +80,8 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Development Status :: 5 - Production/Stable',
